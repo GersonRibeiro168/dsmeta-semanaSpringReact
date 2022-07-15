3da5f39fadac437a0e685d008caf43f0e3c81de9
Gerson Ribeiro <gersonribeiro128@gmail.com>
2022-07-15 19:49:52 -0400

    Twilio SMS

--- backend/src/main/java/com/devsuperior/dsmeta/services/SaleService.java
+++ backend/src/main/java/com/devsuperior/dsmeta/services/SaleService.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.devsuperior.dsmeta.services;
 
 import com.devsuperior.dsmeta.entities.Sales;
@@ -9,7 +5,7 @@ import com.devsuperior.dsmeta.repository.SaleRepository;
 import java.time.Instant;
 import java.time.LocalDate;
 import java.time.ZoneId;
-import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
